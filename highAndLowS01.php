3bb59b032c68d861a375148d4bd4da1ac826c4dd
yuta1127 <51204159+yuta1127@users.noreply.github.com>
2019-07-16 12:35:18 +0900

    Add files via upload

---
+++ highAndLowS01.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type"content="text/html;charset=UTF-8">
+	</head>
+	<body>
+		<center>
+			<?php
+			$leftCard = mt_rand(0,13);
+	           echo"<font size ='6'>High&Lowゲーム</font>";
+	           echo"<hr>";
+	           echo"ランダム数値：".$leftCard;
+	        ?>
+		</center>
+	</body>
+</html>
